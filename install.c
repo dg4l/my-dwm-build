eb74a4a09b701136da45866ded6feaa208e7b4b0
zach <79771590+dg4l@users.noreply.github.com>
2024-09-05 05:48:20 -0400

    modify install.c

--- install.c
+++ install.c
@@ -1,14 +1,22 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
 
 int main(){
 	if (!geteuid()){ // if euid = 0, install
 		system("cd dwm/ && make clean install && cd .. && cd slstatus/ && make clean install && cd .. && cd dmenu/ && make clean install && cd ..");
-		system("cp dwm.desktop /usr/share/xsessions/");
-		system("Done bro...");
+		if (!fopen("/usr/share/xsessions/dwm.desktop", "r")){
+				system("cp dwm.desktop /usr/share/xsessions/");
+		}
+		else{
+			printf("dwm xsession already exists\n");
+		}
+
+		printf("done bro...\n");
 	}
 	else{
-		system("echo please run as root");
+		printf("this program must be run as root\n");
 	}
 	return 0;
 }
